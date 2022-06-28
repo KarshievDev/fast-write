bee041ee8d27a2c94f1cec34f163b148f8edf648
KarshievDev <102017097+KarshievDev@users.noreply.github.com>
2022-06-28 21:58:40 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,126 +1,134 @@
+// elementlarga murojaat
 const word = document.getElementById('word');
 const text = document.getElementById('input_area');
 const scoreEl = document.getElementById('score');
 const timeEl = document.getElementById('time');
 const endGameEl = document.getElementById('end_game');
 
+// so'zlar
 const words = [
-    "instagram",
-    "npm",
-    "vue",
-    "hack",
-    "end",
-    "I am",
-    "all",
-    "number",
-    "oil",
-    "within",
-    "now",
-    "right",
-    "feet",
-    "leave",
-    "what",
-    "now",
-    "facebook",
-    "came",
-    "live",
-    "test",
-    "about",
-    "netflix",
-    "came",
-    "set",
-    "were",
-    "follow",
-    "study",
-    "day",
-    "structure",
-    "over",
-    "why",
-    "why",
-    "talk",
-    "soon",
-    "because",
-    "random",
-    "watch",
-    "year",
-    "her",
-    "any",
-    "snapchat",
-    "I",
-    "both",
-    "around",
-    "book",
-    "line",
-    "mother",
-    "open",
-    "now",
-    "defend",
-    "mile",
-    "go",
-    "by",
-    "found",
-    "said",
-    "eye",
-    "come",
-    "so",
-    "place",
-    "food",
-    "got",
-    "city",
-    "always",
-    "these",
-    "any",
-    "use",
-    "been",
-    "was",
-    "read",
-    "their",
-    "without",
-    "as",
-    "change",
-    "leave",
-    "can",
-    "they",
-    "those",
-    "eat",
-    "never",
-    "no",
-    "eat",
-    "story",
+  "npm cache",
+  "vue",
+  "hacking world",
+  "end",
+  "I",
+  "all",
+  "number",
+  "oil",
+  "within",
+  "now",
+  "right",
+  "feet",
+  "leave",
+  "what",
+  "now",
+  "facebook",
+  "came",
+  "live",
+  "test",
+  "about",
+  "netflix",
+  "came",
+  "set",
+  "were",
+  "follow",
+  "study",
+  "day",
+  "structure",
+  "over",
+  "why",
+  "why",
+  "talk",
+  "soon",
+  "because",
+  "random",
+  "watch",
+  "year",
+  "her",
+  "any",
+  "snapchat",
+  "I",
+  "both",
+  "around",
+  "book",
+  "line",
+  "mother",
+  "open",
+  "now",
+  "defend",
+  "mile",
+  "go",
+  "by",
+  "found",
+  "said",
+  "eye",
+  "come",
+  "so",
+  "place",
+  "food",
+  "got",
+  "city",
+  "always",
+  "these",
+  "any",
+  "use",
+  "been",
+  "was",
+  "read",
+  "their",
+  "without",
+  "as",
+  "change",
+  "leave",
+  "can",
+  "they",
+  "those",
+  "eat",
+  "never",
+  "no",
+  "eat",
+  "story",
 ]
 
+// random so'zlarni chiqarishda yordam beradigan o'zgaruvchi
 let randomWord;
 
+// ochkoni hisoblash uchun o'zgaruvchi
 let score = 0;
 
+// vaqt uchun
 let time = 60;
 
+// random so'zlar ovolamiz
 function getRandomWord() {
   return words[Math.floor(Math.random() * words.length)];
 }
 
+// so'zlarni DOMga qo'shish
 function addToDOM() {
   randomWord = getRandomWord();
   word.innerHTML = randomWord;
 }
 
+// so'zlarni kiritganda ochkoni oshirish va ochkoni yozish
 function updateScore() {
   score++;
   scoreEl.innerHTML = score;
 }
 
+// vaqtni yangilab borish
 const updateTime = () => {
   time--;
   timeEl.innerHTML = time + "s";
 
-  if(time === 0) {
+  if(time === 0) { // vaqt tugaganda xabar chiqishi
     clearInterval(timeInterval);
     gameOver();
   }
 }
 const timeInterval = setInterval(updateTime, 1000);
 
-const gameOver = () => {
+const gameOver = () => { // vaqt tugagandan keyin chiqadigan card
   endGameEl.innerHTML = `
     <h1>Time up!</h1>
     <p>Your final score is : ${score}</p>
@@ -133,10 +141,10 @@ const gameOver = () => {
 
 addToDOM();
 
-
+// input kiritilgan valuelarni olish
 text.addEventListener("input", (e) => { 
   const typedText = e.target.value;
-
+  // valueni o'zgaruvchi tenglashitirb olamiz
 
   if (typedText === randomWord) {
     updateScore(); // hisob oshsin
